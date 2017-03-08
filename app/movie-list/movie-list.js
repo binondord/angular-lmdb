85737227d36b9572bceea1464beaa6b942a7b577
Blanche Bomediano <b.bomediano@arcanys.com>
2017-03-08 14:01:40 +0000

    Removed 'controller' in the .config function. Used  in the controller instead of the Movie factory.

--- app/movie-list/movie-list.js
+++ app/movie-list/movie-list.js
@@ -1,5 +1,5 @@
 'use strict';
-
+var sample = [];
 angular.module('lmdb.movieList', [
   'ngRoute',
   'core.movie'
@@ -8,10 +8,10 @@ angular.module('lmdb.movieList', [
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/list', {
     templateUrl: 'movie-list/movie-list.html',
-    controller: 'MovieListCtrl'
+    //controller: 'MovieListCtrl'
   });
 }])
-
+/*
 .controller('MovieListCtrl', ['Movie', '$rootScope', function(Movie, $rootScope) {
   var vm = this;
   var i;
@@ -24,7 +24,45 @@ angular.module('lmdb.movieList', [
   vm.movies = [];
   vm.movies = Movie.query();
   
+  //$rootScope.$on('MovieAddCtrl:addMovie', angular.bind(this, function (event, data) {
+  $rootScope.$on('MovieAddCtrl:addMovie', function (event, data) {
+    console.log("inside MovieAddCtrl:addMovie");
+    console.log("before push", vm.movies);
+    vm.movies.push({
+      code: "546546",
+      title: "sample",
+      director: "sample",
+      genre: "sample",
+      yearReleased: 1234
+    });
+    vm.movies.map(function(movie){
+        console.log("type of movie item: ", typeof(movie));
+    });
+    console.log("after push", vm.movies);
+  });
+
+  vm.updateTable = function() {
+    sample = vm;
+    vm.movies.push({
+      code: "123456",
+      title: "sample",
+      director: "sample",
+      genre: "sample",
+      yearReleased: 1234
+    });
+  };
+
+}]);
+*/
+.controller('MovieListCtrl', ['$http', '$rootScope', function($http, $rootScope) {
+  var vm = this;
+
+  $http.get('http://192.168.10.10:3000/list').then(function(response) {
+    vm.movies = response.data;
+  });
+
   $rootScope.$on('MovieAddCtrl:addMovie', function(event, args){
+    console.log("inside MovieAddCtrl:addMovie");
     console.log("before push", vm.movies);
     vm.movies.push({
       code: "546546",
@@ -36,31 +74,4 @@ angular.module('lmdb.movieList', [
     console.log("after push", vm.movies);
   });
   
-  vm.updateTable = function() {
-    vm.movies.push({
-      code: "123456",
-      title: "sample",
-      director: "sample",
-      genre: "sample",
-      yearReleased: 1234
-    });
-  };
-
 }]);
-
-/*
-.controller('MovieListCtrl', ['$http', '$rootScope', function($http, $rootScope) {
-  var vm = this;
-
-  $http.get('http://192.168.10.10:3000/list').then(function(response) {
-    vm.movies = response.data;
-    console.log(vm.movies);
-  });
-
-  $rootScope.$on('MovieAddCtrl:addMovie', function(event, args){
-    console.log(vm.movies);
-    vm.movies.push(args);
-  });
-  
-}]);
-*/  
