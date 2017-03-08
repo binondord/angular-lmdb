0bc3b44e20eff77e647f1e214a47d86dfbf400c1
Blanche Bomediano <b.bomediano@arcanys.com>
2017-03-08 13:00:36 +0000

    Initial commit the 'read' part of the CRUD project. No database connection yet.

---
+++ app/movie-list/movie-list.js
@@ -0,0 +1,66 @@
+'use strict';
+
+angular.module('lmdb.movieList', [
+  'ngRoute',
+  'core.movie'
+])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/list', {
+    templateUrl: 'movie-list/movie-list.html',
+    controller: 'MovieListCtrl'
+  });
+}])
+
+.controller('MovieListCtrl', ['Movie', '$rootScope', function(Movie, $rootScope) {
+  var vm = this;
+  var i;
+  
+  // populate the drop-down menu
+  //Movie.query().$promise.then(function(result) {
+  //  vm.movies = result;
+  //});
+
+  vm.movies = [];
+  vm.movies = Movie.query();
+  
+  $rootScope.$on('MovieAddCtrl:addMovie', function(event, args){
+    console.log("before push", vm.movies);
+    vm.movies.push({
+      code: "546546",
+      title: "sample",
+      director: "sample",
+      genre: "sample",
+      yearReleased: 1234
+    });
+    console.log("after push", vm.movies);
+  });
+  
+  vm.updateTable = function() {
+    vm.movies.push({
+      code: "123456",
+      title: "sample",
+      director: "sample",
+      genre: "sample",
+      yearReleased: 1234
+    });
+  };
+
+}]);
+
+/*
+.controller('MovieListCtrl', ['$http', '$rootScope', function($http, $rootScope) {
+  var vm = this;
+
+  $http.get('http://192.168.10.10:3000/list').then(function(response) {
+    vm.movies = response.data;
+    console.log(vm.movies);
+  });
+
+  $rootScope.$on('MovieAddCtrl:addMovie', function(event, args){
+    console.log(vm.movies);
+    vm.movies.push(args);
+  });
+  
+}]);
+*/  
---
+++ app/movie-list/movie-list_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('myApp.movieList module', function() {
+
+  beforeEach(module('myApp.movieList'));
+
+  describe('movieList controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var movieListCtrl = $controller('MovieListCtrl');
+      expect(movieListCtrl).toBeDefined();
+    }));
+
+  });
+});
